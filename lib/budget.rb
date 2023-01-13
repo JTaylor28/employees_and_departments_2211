baee1827f4b7ed044665468194bce095f1d5bd39
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 11:34:22 -0700

    Feat: #department_expenses_under(amount)

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,14 +1,27 @@
 class Budget 
     attr_reader :name,
                 :year,
-                :departments
+                :departments,
+                :employees
+
     def initialize(info)
         @name = info[:name]
         @year = info[:year]
         @departments = []
+        @employees = []
     end
 
     def add_department(department)
         departments << department
     end
+
+    def add_employee(employee)
+        employees << employee
+    end
+
+    def department_expenses_under(amount)
+        department = departments.find_all { |d| d.expenses < amount }
+        department
+    end
+
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe Budget do
         
         it "returns a list of departments and starts with none" do
             expect(co_budget.departments).to eq([])
+            
             co_budget.add_department(customer_service)
             co_budget.add_department(human_resources)
             expect(co_budget.departments).to eq([customer_service, human_resources])
@@ -33,9 +34,25 @@ RSpec.describe Budget do
     describe "#employees & #add_employee(employee)" do
         it " returns a list of employees and starts with none" do
             expect(co_budget.employees).to eq([])
+            
             co_budget.add_employee(bobbi)
             co_budget.add_employee(aaron)
             expect(co_budget.employees).to eq([bobbi, aaron])
+        end
+    end
+
+    describe "#department_expenses_under(amount)" do 
+        it "resutens a list of departments with expenses under the given amount" do
+            expect(co_budget.department_expenses_under(500)).to eq([])
+            
+            co_budget.add_department(customer_service)
+            co_budget.add_department(human_resources)
+            customer_service.expense(100)
+            human_resources.expense(100)
+            expect(co_budget.department_expenses_under(500)).to eq([customer_service, human_resources])
+            
+            customer_service.expense(500)
+            expect(co_budget.department_expenses_under(500)).to eq([human_resources])
         end
     end
 end
