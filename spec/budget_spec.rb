b95b5040081f7131259f9d0b14cb393c40cb39c9
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 10:43:03 -0700

    Test: #Departments & #add_department

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -5,6 +5,7 @@ require './lib/budget'
 RSpec.describe Budget do
     let(:co_budget) {Budget.new(name: 'CO Budget', year: "2023" )}
     let(:customer_service) {Department.new("Customer Service")}
+    let(:human_resources) {Department.new("Human Resources")}
     let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
     let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
 
@@ -18,4 +19,11 @@ RSpec.describe Budget do
             expect(co_budget.year).to eq("2023")
         end
     end 
+        
+    describe "#departments & #add_department(department)" do
+        it "returns a list of departments and starts with none" do
+            expect(co_budget.departments).to eq([])
+
+        end
+    end
 end
