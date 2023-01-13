374987f6e7a358d888c57287d0f7c8d76ca1f3f2
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 10:50:50 -0700

    Test: #employees & #add_employee(employee)

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -27,7 +27,15 @@ RSpec.describe Budget do
             co_budget.add_department(customer_service)
             co_budget.add_department(human_resources)
             expect(co_budget.departments).to eq([customer_service, human_resources])
+        end
+    end
 
+    describe "#employees & #add_employee(employee)" do
+        it " returns a list of employees and starts with none" do
+            expect(co_budget.employees).to eq([])
+            co_budget.add_employee(bobbi)
+            co_budget.add_employee(aaron)
+            expect(co_budget.employees).to eq([bobbi, aaron])
         end
     end
 end
