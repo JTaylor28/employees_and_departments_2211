92004d355d24862165bc37afad12fb73a48cc031
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 09:21:13 -0700

    Test: has attributes

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Employee do
         it "exists" do
             expect(bobbi).to be_an_instance_of(Employee)
         end
+
+        it "has attributes" do
+            expect(bobbi.name).to eq("Bobbi Jaeger")
+            expect(bobbi.age).to eq(30)
+            expect(bobbi.salary).to eq(100000)
+        end 
+
     end
         
 end
