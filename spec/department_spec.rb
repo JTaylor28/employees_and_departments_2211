bec7c98d0f372b6a6a94b6d96f68543da3e9f956
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 09:44:14 -0700

    Teats: it starts with no employees

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -10,10 +10,15 @@ RSpec.describe Department do
             expect(customer_service).to be_an_instance_of(Department)
         end
 
-        it "has attribues" do
+        it "has attributes" do
             expect(customer_service.name). to eq("Customer Service")
         end
-    end 
+
+        it "has starts with no employees" do 
+            expect(customer_service.employees).to ([])
+        end
+    end
+
 
 end
 
