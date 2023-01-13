7f85547a56350e14fdb00d80822efe8536d98663
James Taylor <jrtaylor0614@gmail.com>
2023-01-13 09:13:18 -0700

    Feat: initialize class

---
+++ lib/employee.rb
@@ -0,0 +1,8 @@
+class Employee 
+    
+    def initialize(info)
+        @name = info[:name]
+        @age = info[:age]
+        @salary = info[:salary]
+    end
+end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,11 +3,11 @@ require './lib/employee'
 
 RSpec.describe Employee do
 
-    let(bobbi)  {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+    let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
 
     describe '#initialize' do
         it "exists" do
-            expect(boobi).to be_an_instance_of(Employee)
+            expect(bobbi).to be_an_instance_of(Employee)
         end
     end
         
